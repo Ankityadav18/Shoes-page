a7584046b0973ae00c400e15b213fb9dcf09bad8
Ankityadav18 <140178073+Ankityadav18@users.noreply.github.com>
2025-01-13 17:58:23 +0530

    shoes page

---
+++ src/Component/NavBar.jsx
@@ -0,0 +1,24 @@
+import Button from "./Button";
+
+
+function NavBar(){
+return (
+    <div className="header">
+      <p><img src="https://s3-alpha-sig.figma.com/img/4517/5a94/d5389cc06b9d40c646878c60ac4bf7ed?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kVL3iPffTrc~ucwmUAFVVi5SoeBsi71NgFWSnhCj3ivFbvTJaCRntIR0Vrmy88ZiqwjgkuWtaoNoC0URvYt6hC3qYihQ-IPO6tTmF~gjkgwTBZjS0Zn1TOEedbVtTuAswMAeRgu-848t5WfbrflV290QZeuKFVg3DZIsHeHupqjrWdqNDRMgdYbk7lOxHsstPddrY~r2UaK3mmdoLb7t4N7fCCOas6e4zGFte~h7xE8lICEWpDCG-XNxZJAlA6ujclTNepqOAOPI~Mwx7V2THiC~2c4WoHbQ71VTUq7YbqlHVmM3Gp0TLXu2IAlOxzBRjHsHrUTJ2HAIMaEGR-QLLQ__"></img></p>
+    <div className="header1">
+    <p className="para1">Menu</p>
+    <p className="para2">Contact</p>
+    <p className="para3">Location</p>
+    <p className="para4">Details</p>
+    </div>
+    <Button >login</Button>
+    </div>
+)
+
+
+
+
+
+
+}
+export default NavBar;
---
+++ src/Component/Section.jsx
@@ -0,0 +1,31 @@
+import Button from "./Button";
+
+function Section() {
+
+    return (
+        <>
+            <div className="Section">
+                <div className="left">
+                    <h1 className="head">YOUR FEET
+                        DESERVE
+                        THE BEST</h1>
+
+                    <p className="paragraph">YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+                    <Button>ShopNow</Button>
+                    <Button>Category</Button>
+                    <p className="paraend">Also available On</p>
+                    <p><img src="https://s3-alpha-sig.figma.com/img/4331/d1d3/fe15236a1a9843e35f7b3b8467e998ff?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jv27QX8oYlvglXFFShxAhDkNLay0ZJKFTpxyKlZNXBWJjy6YI~nEgj9LJ9qn7M~9L1ojsK3zjSGGYQDG-24U7hrbEYx1dE82J4OLN8QneioylOHGtmUTl8ibTtG3GdKoyjgwBXY4rqfB0nI2tFN-xeiWa8bUIKdfTFLNXXfSBotbag6~tw13ImtksT~Kp6sv8ga53eQV75XRWPVKAD~zNQJsgiQALbjWvGLMSXQEiqxAfZJNeFxWeJUgid1fkgVBnbnmqPBLoQKxCX4gK7WdzBHZMq-xEckEIY9jDWqM8SoNdq5fCLyWcO9LxsF5tI8dq~PPbJr7PaXA5~fFJUgp6A__"></img><span><img src="https://s3-alpha-sig.figma.com/img/9669/ba8f/4f98c0bc6952e9281673372fcb16cfc0?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JdI9~6j8~q0pK-jn6FaQ3fS3ZM0FB~06~N2be1MBZUWUipjkiNlcAI98oFyFC9Ly7vcx1s8RbxDDDlahCzkvF3QTsd~nNXjb8z93hoNJchvggm4vaptn0Bw2hP1P0LREbCGr0ltsR4~uPA2sAosgeNUNpKxpW1AtSvyUfblQyvMeXHA-okC4maoAk6Vqv2Y8O4oeH3XSvFQgY~ZHb-SzHGSEzCTRnwqPf0~0SgFxu7gmPH13eU5gTOdQtPFSvWRZYiIlBiixMPxbs8YeHGEV-I0KpaoOnw5o5ZXma9ZEh0visC0JJYKlnOrQqUxAcJYuS-KaJWL-94F37qkaJr0LLQ__"></img></span></p>
+                </div>
+                <div className="right">
+                    <img className="shoes" src="https://s3-alpha-sig.figma.com/img/ccd9/e7f6/9d4cbfcf331c69b8e21db35d2e158410?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=IiebJEd9DP9P-wYQvHOaIHQvNpSFmQijRPGIbp0AQpi7vwgbhyJFWs97-n6X8IYUDGois~qg5UjwEEAGNJQJ28Cd1xKCsUd7lcjDQ6fTn2ytu6BcMm2raDT1WVtWMRRkdIhT4Bv1-MwFb9~ngEsqP2w9b8ofoK502qZA1dKOJke8e3VNmJVnrhuU~PmhXowyBSAiLZ6oJI-IBRxlf7l1WEEOFE7cw7J6LbneaWyGYX4WamZSOio3fviqBNRKf~dK4z1BQDjo3q4osgQIQM-KedQcoLbo89CwaNtBZ86NpTnP~8~VO-tgQ913Hzdhpj0uxosOEbjMxlRIrakQhUypTw__"></img>
+
+                </div>
+            </div>
+
+
+
+        </>
+    )
+
+}
+export default Section;
